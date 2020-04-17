48ce3c7fd950ec4a67cb314cae197e0e403db64c
zhaozheZhang <506549503@qq.com>
2020-04-17 20:48:44 +0800

    前端

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,14 +1,13 @@
-//package com.example.demo.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class IndexController {
-//
-//    @RequestMapping("/")
-//    public String getIndex(){
-//        return "index";
-//    }
-//}
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+}
